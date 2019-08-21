d3a6cd0af727d1261d5402aaa7dd4eca9095e5de
jrc03c <jrc03c@pm.me>
2019-08-21 10:26:22 -0500

    Fixed punctuation stripper.

--- index.js
+++ index.js
@@ -1,104 +1,104 @@
-let mathjs = require("mathjs");
+let mathjs = require("mathjs")
 
 let gt = {
 	string: {
 		stripPunctuation: function(string){
-			let punct = "`~!@#$%^&*()_-+=[{]}\\|;:,<.>/?'" + '"';
-			let out = string;
+			let valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 "
+			let out = ""
 
-			for (let i=0; i<punct.length; i++){
-				let p = punct[i];
-				while (out.includes(p)) out = out.replace(p, "");
+			for (let i=0; i<string.length; i++){
+				let char = string[i]
+				if (valid.includes(char)) out += char
 			}
 
-			return out;
+			return out
 		},
 
 		toCamelCase: function(string){
-			let array = gt.string.stripPunctuation(string).split(" ");
-			let out = array[0].toLowerCase();
+			let array = gt.string.stripPunctuation(string).split(" ")
+			let out = array[0].toLowerCase()
 
 			for (let i=1; i<array.length; i++){
-				let s = array[i];
-				out += s[0].toUpperCase() + s.slice(1, s.length).toLowerCase();
+				let s = array[i]
+				out += s[0].toUpperCase() + s.slice(1, s.length).toLowerCase()
 			}
 
-			return out;
+			return out
 		},
 	},
 
 	array: {
 		shuffle: function(array, seed){
-			mathjs.config({randomSeed: seed});
-			let out = array.slice();
+			mathjs.config({randomSeed: seed})
+			let out = array.slice()
 
 			for (let i=0; i<out.length; i++){
-				let j = Math.floor(mathjs.random() * out.length);
-				let k = Math.floor(mathjs.random() * out.length);
-				let buffer = out[j];
-				out[j] = out[k];
-				out[k] = buffer;
+				let j = Math.floor(mathjs.random() * out.length)
+				let k = Math.floor(mathjs.random() * out.length)
+				let buffer = out[j]
+				out[j] = out[k]
+				out[k] = buffer
 			}
 
-			return out;
+			return out
 		},
 
 		toSet: function(array){
-			let out = [];
+			let out = []
 
 			array.forEach(function(item){
 				if (out.indexOf(item) < 0){
-					out.push(item);
+					out.push(item)
 				}
-			});
+			})
 
-			return out;
+			return out
 		},
 	},
 
 	object: {
 		toAssociation(obj){
 			function recursiveParse(obj){
-				let type = typeof(obj);
+				let type = typeof(obj)
 
-				if (type === "string") return JSON.stringify(obj);
-				if (type === "number") return obj;
+				if (type === "string") return JSON.stringify(obj)
+				if (type === "number") return obj
 
-				let pairs = [];
+				let pairs = []
 
 				Object.keys(obj).forEach(function(key){
-					let val = recursiveParse(obj[key]);
-					pairs.push(`"` + key + `"->` + val);
-				});
+					let val = recursiveParse(obj[key])
+					pairs.push(`"` + key + `"->` + val)
+				})
 
-				return "{" + pairs.join(",") + "}";
+				return "{" + pairs.join(",") + "}"
 			}
 
-			return recursiveParse(obj);
+			return recursiveParse(obj)
 		}
 	},
 
 	template: {
 		build: function(templateString, variableDict){
 			// variable syntax: {$ variable $}
-			let out = templateString;
-			let rx = /\{\$ ?(.*?) ?\$\}/g;
-			placeholders = templateString.match(rx);
+			let out = templateString
+			let rx = /\{\$ ?(.*?) ?\$\}/g
+			placeholders = templateString.match(rx)
 
-			if (!placeholders) return out;
+			if (!placeholders) return out
 
 			placeholders.forEach(function(placeholder){
-				let abbrev = placeholder.split(" ").join("").replace("{$", "").replace("$}", "");
-				if (!variableDict[abbrev]) throw "No definition for " + abbrev + ".";
+				let abbrev = placeholder.split(" ").join("").replace("{$", "").replace("$}", "")
+				if (!variableDict[abbrev]) throw "No definition for " + abbrev + "."
 
 				while (out.includes(placeholder)){
-					out = out.replace(placeholder, variableDict[abbrev]);
+					out = out.replace(placeholder, variableDict[abbrev])
 				}
-			});
+			})
 
-			return out;
+			return out
 		},
 	},
-};
+}
 
-module.exports = gt;
+module.exports = gt
