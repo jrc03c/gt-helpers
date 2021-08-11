aeb5ee35d10bec90e1125004f3b360dd22ba28f8
jrc03c <jrc03c@pm.me>
2021-08-11 08:51:28 -0500

    Fixed the js-math-tools dependency path.

--- index.js
+++ index.js
@@ -1,112 +1,125 @@
-let tools = require("js-math-tools")
+let tools = require("@jrc03c/js-math-tools")
 let Liquid = require("liquidjs").Liquid
 let liquid = new Liquid()
 
 let gt = {
-	date: {
-		toGTDateObject: function(date){
-			let out = {
-				"year": date.getFullYear(),
-				"month": date.getMonth()+1,
-				"day": date.getDate(),
-				"hour": date.getHours(),
-				"minute": date.getMinutes(),
-			}
+  date: {
+    toGTDateObject: function (date) {
+      let out = {
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        day: date.getDate(),
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+      }
 
-			return out
-		},
-	},
+      return out
+    },
+  },
 
-	string: {
-		stripPunctuation: function(string){
-			let valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 \t\n\r"
-			let out = ""
+  string: {
+    stripPunctuation: function (string) {
+      let valid =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 \t\n\r"
+      let out = ""
 
-			for (let i=0; i<string.length; i++){
-				let char = string[i]
-				if (valid.includes(char)) out += char
-			}
+      for (let i = 0; i < string.length; i++) {
+        let char = string[i]
+        if (valid.includes(char)) out += char
+      }
 
-			while (out.includes("\t")) out = out.replace("\t", " ")
-			while (out.includes("\n")) out = out.replace("\n", " ")
-			while (out.includes("\r")) out = out.replace("\r", " ")
-			while (out.includes("  ")) out = out.replace("  ", " ")
+      while (out.includes("\t")) out = out.replace("\t", " ")
+      while (out.includes("\n")) out = out.replace("\n", " ")
+      while (out.includes("\r")) out = out.replace("\r", " ")
+      while (out.includes("  ")) out = out.replace("  ", " ")
 
-			return out
-		},
+      return out
+    },
 
-		toCamelCase: function(string){
-			let array = gt.string.stripPunctuation(string).split(" ").filter(s => s.length > 0)
-			let out = array[0].toLowerCase()
+    toCamelCase: function (string) {
+      let array = gt.string
+        .stripPunctuation(string)
+        .split(" ")
+        .filter(s => s.length > 0)
+      let out = array[0].toLowerCase()
 
-			for (let i=1; i<array.length; i++){
-				let s = array[i]
-				out += s[0].toUpperCase() + s.slice(1, s.length).toLowerCase()
-			}
+      for (let i = 1; i < array.length; i++) {
+        let s = array[i]
+        out += s[0].toUpperCase() + s.slice(1, s.length).toLowerCase()
+      }
 
-			return out
-		},
-	},
+      return out
+    },
+  },
 
-	array: {
-		shuffle: function(array, seed){
-			tools.math.seed(seed)
-			return tools.math.shuffle(array)
-		},
+  array: {
+    shuffle: function (array, seed) {
+      tools.math.seed(seed)
+      return tools.math.shuffle(array)
+    },
 
-		toSet: function(array){
-			return tools.math.set(array)
-		},
-	},
+    toSet: function (array) {
+      return tools.math.set(array)
+    },
+  },
 
-	object: {
-		toAssociation(obj){
-			function recursiveParse(obj){
-				let type = typeof(obj)
+  object: {
+    toAssociation(obj) {
+      function recursiveParse(obj) {
+        let type = typeof obj
 
-				if (type === "string") return JSON.stringify(obj)
-				if (type === "number") return obj
+        if (type === "string") return JSON.stringify(obj)
+        if (type === "number") return obj
 
-				let pairs = []
+        let pairs = []
 
-				Object.keys(obj).forEach(function(key){
-					let val = recursiveParse(obj[key])
-					pairs.push(`"` + key + `" -> ` + val)
-				})
+        Object.keys(obj).forEach(function (key) {
+          let val = recursiveParse(obj[key])
+          pairs.push(`"` + key + `" -> ` + val)
+        })
 
-				return "{" + pairs.join(", ") + "}"
-			}
+        return "{" + pairs.join(", ") + "}"
+      }
 
-			return recursiveParse(obj)
-		}
-	},
+      return recursiveParse(obj)
+    },
+  },
 
-	template: {
-		build: function(templateString, variableDict){
-			// variable syntax: {$ variable $}
-			let out = templateString
-			let rx = /\{\$ ?(.*?) ?\$\}/g
-			placeholders = templateString.match(rx)
+  template: {
+    build: function (templateString, variableDict) {
+      // variable syntax: {$ variable $}
+      let out = templateString
+      let rx = /\{\$ ?(.*?) ?\$\}/g
+      placeholders = templateString.match(rx)
 
-			if (!placeholders) return out
+      if (!placeholders) return out
 
-			placeholders.forEach(function(placeholder){
-				let abbrev = placeholder.split(" ").join("").replace("{$", "").replace("$}", "")
-				if (!variableDict[abbrev]) throw "No definition for " + abbrev + "."
+      placeholders.forEach(function (placeholder) {
+        let abbrev = placeholder
+          .split(" ")
+          .join("")
+          .replace("{$", "")
+          .replace("$}", "")
+        if (!variableDict[abbrev]) throw "No definition for " + abbrev + "."
 
-				while (out.includes(placeholder)){
-					out = out.replace(placeholder, variableDict[abbrev])
-				}
-			})
+        while (out.includes(placeholder)) {
+          out = out.replace(placeholder, variableDict[abbrev])
+        }
+      })
 
-			return out
-		},
+      return out
+    },
 
-		liquidBuild: async function(templateString, variableDict){
-			return await liquid.parseAndRender(templateString, variableDict)
-		},
-	},
+    liquidBuild: async function (templateString, variableDict) {
+      return await liquid.parseAndRender(templateString, variableDict)
+    },
+  },
 }
 
-try {module.exports = gt} catch(e){}
-try {window.gt = gt} catch(e){}
+try {
+  module.exports = gt
+} catch (e) {}
+
+try {
+  window.gt = gt
+} catch (e) {}
