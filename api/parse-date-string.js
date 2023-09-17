59c437d256e573b264d5cb32ab4ec5cf651779d7
jrc03c <jrc03c@pm.me>
2023-09-16 20:12:08 -0500

    ...

--- api/parse-date-string.js
+++ api/parse-date-string.js
@@ -1,13 +1,19 @@
 module.exports = function (request, response) {
 	try {
 		const date = new Date(request.query.date_string)
+
+		if (date.toString() === "Invalid Date") {
+			throw new Error(
+				"The date string you provided was not in a date format we recognized!"
+			)
+		}
+
 		const year = date.getFullYear()
 		const month = date.getMonth() + 1
 		const day = date.getDate()
 		const hour = date.getHours()
 		const minute = date.getMinutes()
 		const second = date.getSeconds()
-		const millisecond = date.getMilliseconds()
 
 		return response.json({
 			year,
@@ -16,7 +22,6 @@ module.exports = function (request, response) {
 			hour,
 			minute,
 			second,
-			millisecond,
 		})
 	} catch (e) {
 		return response.status(500).send(e.toString())
